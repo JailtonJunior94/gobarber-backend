a4b0602b19e61ffda4717d014a13b27834a264a4
JailtonJunior94 <jailton.junior94@outlook.com>
2020-05-27 19:48:25 -0300

    Finalização do módulo: Arquitetura e DDD

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "@typescript-eslint/camelcase": "off",
+    "no-useless-constructor": "off",
     "@typescript-eslint/no-unused-vars": ["error", {
       "argsIgnorePattern": "_"
     }],
